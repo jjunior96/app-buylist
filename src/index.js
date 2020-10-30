fc67aa35983abab597bda0d40b71ede1e26dd839
Junior Alves <jjalves96@gmail.com>
2020-10-30 16:59:00 -0300

    chore: create styles for main component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
 
 export default function App() {
-  return <View />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello GoStack</Text>
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
