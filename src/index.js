752f5bd4a169841a5ddcea03f5514c0131aa87c7
Junior Alves <jjalves96@gmail.com>
2020-11-02 09:17:37 -0300

    chore: create styles for main component

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eee',
+    backgroundColor: '#eee', // altera cor de fundo
     justifyContent: 'center',
     alignItems: 'center',
   },
