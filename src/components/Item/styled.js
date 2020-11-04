5abea9f743a97c56261a274ee61027a8cbe5fcf2
Junior Alves <jjalves96@gmail.com>
2020-11-04 09:29:49 -0300

    feat: create Item component

---
+++ src/components/Item/styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Title = styled.Text`
+  font-size: 16px;
+  color: #fff;
+`;
+
+export const Button = styled.TouchableOpacity`
+  background-color: #fa8231;
+  width: 2.4rem;
+  height: 2.4rem;
+  border-radius: 50px;
+  padding: 6px 16px;
+`;
+
+export const FlatList = styled.FlatList`
+  background-color: red;
+  height: 20px;
+  width: 200px;
+`;
